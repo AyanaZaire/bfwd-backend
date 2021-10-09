1fde46f2fa1b8e98c4a466dbec893ef79d92495a
AyanaZaire <ayanazaire@gmail.com>
2021-10-09 18:20:34 -0400

    revert cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://www.blackfeminist.world/'
+    origins '*'
 
     resource '*',
       headers: :any,
